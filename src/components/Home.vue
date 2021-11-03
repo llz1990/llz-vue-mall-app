7ea229e056495cb9ea7390b92c988f215f16bb54
llz1990 <754960880@qq.com>
2021-11-03 23:08:53 +0800

    初始化项目

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home-container">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.home-container{
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  font-size: 0.14rem;
+}
+
+</style>
